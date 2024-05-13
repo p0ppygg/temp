e37b7dae2a62e59d040985e69cc9b4ac67fa3cef
p0ppygg <139044139+p0ppygg@users.noreply.github.com>
2024-05-13 10:31:47 +0700

    Update alex.php

--- alex.php
+++ alex.php
@@ -5,7 +5,7 @@ error_reporting(0);
 
 set_time_limit(0);
 //@set_magic_quotes_runtime(0);
-//@clearstatcache();
+@clearstatcache();
 @ini_set('error_log',NULL);
 @ini_set('log_errors',0);
 @ini_set('max_execution_time',0);
