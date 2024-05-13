3189845945092e4762d697a3974ed88a1108563d
p0ppygg <139044139+p0ppygg@users.noreply.github.com>
2024-05-13 09:42:08 +0700

    Update GELAP.php

--- GELAP.php
+++ GELAP.php
@@ -3,14 +3,14 @@ session_start();
 error_reporting(0);
 // IF SHELL DOESN'T WORK, REMOVE THE CODE BELOW
 
-//set_time_limit(0);
+set_time_limit(0);
 //@set_magic_quotes_runtime(0);
-//@clearstatcache();
-//@ini_set('error_log',NULL);
-//@ini_set('log_errors',0);
-//@ini_set('max_execution_time',0);
-//@ini_set('output_buffering',0);
-//@ini_set('display_errors', 0);
+@clearstatcache();
+@ini_set('error_log',NULL);
+@ini_set('log_errors',0);
+@ini_set('max_execution_time',0);
+@ini_set('output_buffering',0);
+@ini_set('display_errors', 0);
 
 // END UP HERE
 
